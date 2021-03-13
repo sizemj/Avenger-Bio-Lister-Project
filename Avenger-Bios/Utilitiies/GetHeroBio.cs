b7fac634d511f2d476108585e37350130d1f47b5
Jason Sizemore <jsizemore1@outlook.com>
2021-03-12 23:49:02 -0500

    closer

--- Avenger-Bios/Utilitiies/GetHeroBio.cs
+++ Avenger-Bios/Utilitiies/GetHeroBio.cs
@@ -16,27 +16,24 @@ namespace AvengersBioLister.Utilitiies
         public static async Task PullBioAsync(int heroSelection)
         {
             var access_token = Environment.GetEnvironmentVariable("SuperHeroAPI_AccessToken");
-            var url = "https://www.superheroapi.com/api.php/";
+            var baseUrl = "https://www.superheroapi.com/api.php/";
+            var client = new HttpClient();
 
-            HttpClient client = new HttpClient();
-            //Not working here??
-            HttpResponseMessage response = await client.GetAsync($"{url}{access_token}/30");
-            var responseBody = await response.Content.ReadAsStringAsync();
-            var antManBio = JsonConvert.DeserializeObject<Avenger>(File.ReadAllText(responseBody));
 
-            //switch (heroSelection)
-            //{
-            //    case 1:
-            //        HttpResponseMessage response = await client.GetAsync($"{url}{access_token}/30");
-            //        var responseBody = await response.Content.ReadAsStringAsync();
-            //        var antManBio = JsonConvert.DeserializeObject<Avenger>(File.ReadAllText(responseBody));
-            //        Console.WriteLine(antManBio);
-            //        break;
 
-            //    default:
-            //        Console.WriteLine("Did not work!");
-            //        break;
-            //}
+            switch (heroSelection)
+            {
+                case 1:
+                    var response = client.GetAsync($"{baseUrl}{access_token}/30").Result;
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    var antManBio = JsonConvert.DeserializeObject<Avenger>(File.ReadAllText(responseBody));
+                    Console.WriteLine(antManBio);
+                    break;
+
+                default:
+                    Console.WriteLine("Did not work!");
+                    break;
+            }
         }
 
 
